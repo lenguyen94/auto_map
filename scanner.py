b158fd8c15e0262271ae49568b0258d25eb83c41
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2019-12-22 16:53:30 +0800

    Removed unnecessary import statement Simplified call to replace 0's with nan's Changed function names from listener to scanner to better reflect the function

--- scanner.py
+++ scanner.py
@@ -1,71 +1,42 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
-
-## Simple talker demo that listens to std_msgs/Strings published 
-## to the 'chatter' topic
 
 import rospy
 import numpy as np
-from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan
 
+
 def callback(msg):
+	# create numpy array
 	laser_range = np.array([msg.ranges])
 	# replace 0's with nan
 	lr2 = laser_range
 	lr2[lr2==0] = np.nan
-	# lr2 = (laser_range==0).choose(laser_range,np.nan)
 	# find index with minimum value
 	lr2i = np.nanargmin(lr2)
+	
+	# log the info
     	rospy.loginfo('Shortest distance is %i degrees', lr2i)
 
-def listener():
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('scanner', anonymous=True)
+def scanner():
+	# initialize node
+	rospy.init_node('scanner', anonymous=True)
 
-    rate = rospy.Rate(1) # 10hz
-    rospy.Subscriber('scan', LaserScan, callback)
-    rate.sleep()
+	# set the update rate to 1 Hz
+	rate = rospy.Rate(1) # 1 Hz
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+	# subscribe to LaserScan data
+	rospy.Subscriber('scan', LaserScan, callback)
+    
+	# wait until it is time to run again
+	rate.sleep()
+
+	# spin() simply keeps python from exiting until this node is stopped
+	rospy.spin()
+
 
 if __name__ == '__main__':
-    listener()
+    try:
+	    scanner()
+    except  rospy.ROSInterruptException:
+        pass
