0c5d340cba663870132f352753e0dfa4e5100fa8
Yen Shih Cheng <eleys@nus.edu.sg>
2019-12-22 17:00:33 +0800

    New function that tracks Odometry in order to rotate by specified angles.

---
+++ mover2.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+from std_msgs.msg import String
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Twist
+import math
+
+roll = pitch = yaw = 0.0
+
+def isnumber(value):
+	try:
+		val = int(value)
+		return True
+	except ValueError:
+		return False
+		
+		
+def rotatebot(angle)
+	# create Twist object
+    twist = Twist()
+    # set up Publisher to cmd_vel topic
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    # set the update rate
+    rate = rospy.Rate(5) # 5 Hz
+
+	# get current yaw angle
+	current_yaw = yaw
+	# calculate desired yaw
+	desired_yaw = yaw + math.radians(angle)
+	# set linear speed to zero so the TurtleBot rotates on the spot
+	# check which direction we should rotate
+	if(angle>0)
+		twist.angular.z = 1.0
+	else
+		twist.angular.z = -1.0
+	
+	# rotate until yaw angle exceeds yaw+angle
+	pub.publish(twist)
+
+	# change twist object to stop rotation
+	twist.angular.z = 0.0
+	
+	while(yaw < desired_yaw)
+        rate.sleep()
+
+	# stop rotation
+	pub.publish(twist)
+
+
+def get_rotation (msg):
+	orientation_quat =  msg.pose.pose.orientation	
+	orientation_list = [orientation_quat.x, orientation_quat.y, orientation_quat.z, orientation_quat.w]
+	(roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+
+
+def listener():
+    rate = rospy.Rate(1) # 10hz
+    rospy.Subscriber('odom', Odometry, get_rotation)
+    rate.sleep()
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
+def mover2():
+    twist = Twist()
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    rospy.init_node('mover', anonymous=True)
+    rate = rospy.Rate(1) # 1 Hz
+    
+    while not rospy.is_shutdown():
+    	cmd_char = str(raw_input("Keys w/x -/+int s: "))
+    	
+    	if isnumber(cmd_char)
+    		# rotate by specified angle
+    		rotatebot(int(cmd_char))
+    	else		
+			if cmd_char == 's':
+				twist.linear.x = 0.0
+				twist.angular.z = 0.0
+			elif cmd_char == 'w':
+				twist.linear.x = 0.1
+				twist.angular.z = 0.0
+			elif cmd_char == 'x':
+				twist.linear.x = -0.1
+				twist.angular.z = 0.0
+
+			pub.publish(twist)
+
+        rospy.loginfo(cmd_char)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        mover2()
+    except rospy.ROSInterruptException:
+        pass
