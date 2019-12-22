f221d8527d6142cbf8f9026ea07e256867938c79
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2019-12-22 16:50:27 +0800

    Removed unnecessary import statement Changed function name to mover instead of talker Added comments to explain what each line does

--- mover.py
+++ mover.py
@@ -1,77 +1,60 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
-
-## Simple talker demo that published std_msgs/Strings messages
-## to the 'chatter' topic
 
 import rospy
-#from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
-def talker():
+
+def mover():
+    # create Twist object
     twist = Twist()
 
+    # set up Publisher to cmd_vel topic
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 
+    # initialize node
     rospy.init_node('mover', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    # set the update rate
+    rate = rospy.Rate(10) # 10 Hz
+
+    # while not CTRL-C
     while not rospy.is_shutdown():
+    	# get keyboard input
     	cmd_char = str(raw_input("Keys w/x a/d s: "))
+    	
+    	# check which key was entered
     	if cmd_char == 's':
+            # stop moving
 	    twist.linear.x = 0.0
 	    twist.angular.z = 0.0
     	elif cmd_char == 'w':
+            # move forward
 	    twist.linear.x = 0.1
 	    twist.angular.z = 0.0
     	elif cmd_char == 'x':
+            # move backward
 	    twist.linear.x = -0.1
 	    twist.angular.z = 0.0
     	elif cmd_char == 'a':
+            # turn counter-clockwise
 	    twist.linear.x = 0.0
 	    twist.angular.z = 1.0
     	elif cmd_char == 'd':
+            # turn clockwise
 	    twist.linear.x = 0.0
 	    twist.angular.z = -1.0
 
+	# publish twist to move the TurtleBot
 	pub.publish(twist)
 
+	# log the info
         rospy.loginfo(cmd_char)
+        
+        # wait until it is time to run again
         rate.sleep()
+
 
 if __name__ == '__main__':
     try:
-        talker()
+        mover()
     except rospy.ROSInterruptException:
         pass
