bcf669f7bc5cb419a0b38169f1abb77f025c6c1e
Shih-Cheng Yen <yenshihcheng@gmail.com>
2019-12-23 09:32:13 +0800

    Fixed code to print yaw value.

--- mover2.py
+++ mover2.py
@@ -11,52 +11,52 @@ import math
 roll = pitch = yaw = 0.0
 
 def isnumber(value):
-	try:
-		int(value)
-		return True
-	except ValueError:
-		return False
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
 
 
 def rotatebot(rot_angle):
-	# create Twist object
-	twist = Twist()
-	# set up Publisher to cmd_vel topic
-	pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-	# set the update rate to 5 Hz
-	rate = rospy.Rate(5)
+    # create Twist object
+    twist = Twist()
+    # set up Publisher to cmd_vel topic
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    # set the update rate to 5 Hz
+    rate = rospy.Rate(5)
 
-	# get current yaw angle
-	current_yaw = yaw
+    # get current yaw angle
+    current_yaw = yaw
     # log the info
-    rospy.loginfo(['Current: ' current_yaw])
-	# calculate desired yaw
-	desired_yaw = current_yaw + math.radians(rot_angle)
-	# set linear speed to zero so the TurtleBot rotates on the spot
-	twist.linear.x = 0.0
-	# check which direction we should rotate
-	if(rot_angle>0):
-		twist.angular.z = 1.0
-	else:
-		twist.angular.z = -1.0
+    rospy.loginfo(['Current: ' + str(current_yaw)])
+    # calculate desired yaw
+    desired_yaw = current_yaw + math.radians(rot_angle)
+    # set linear speed to zero so the TurtleBot rotates on the spot
+    twist.linear.x = 0.0
+    # check which direction we should rotate
+    if(rot_angle>0):
+        twist.angular.z = 1.0
+    else:
+        twist.angular.z = -1.0
 
-	# rotate until yaw angle exceeds yaw+angle
-	pub.publish(twist)
+    # rotate until yaw angle exceeds yaw+angle
+    pub.publish(twist)
 
-	# change twist object to stop rotation
-	twist.angular.z = 0.0
+    # change twist object to stop rotation
+    twist.angular.z = 0.0
 
-	while(yaw < desired_yaw):
-        	rate.sleep()
+    while(yaw < desired_yaw):
+        rate.sleep()
 
-	# stop rotation
-	pub.publish(twist)
+    # stop rotation
+    pub.publish(twist)
 
 
 def get_rotation (msg):
-	orientation_quat =  msg.pose.pose.orientation
-	orientation_list = [orientation_quat.x, orientation_quat.y, orientation_quat.z, orientation_quat.w]
-	(roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+    orientation_quat =  msg.pose.pose.orientation
+    orientation_list = [orientation_quat.x, orientation_quat.y, orientation_quat.z, orientation_quat.w]
+    (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
 
 
 def listener():
@@ -74,23 +74,23 @@ def mover2():
     rate = rospy.Rate(1) # 1 Hz
 
     while not rospy.is_shutdown():
-    	cmd_char = str(raw_input("Keys w/x -/+int s: "))
+        cmd_char = str(raw_input("Keys w/x -/+int s: "))
 
-    	if isnumber(cmd_char):
-    		# rotate by specified angle
-    		rotatebot(int(cmd_char))
-    	else:
-			if cmd_char == 's':
-				twist.linear.x = 0.0
-				twist.angular.z = 0.0
-			elif cmd_char == 'w':
-				twist.linear.x = 0.1
-				twist.angular.z = 0.0
-			elif cmd_char == 'x':
-				twist.linear.x = -0.1
-				twist.angular.z = 0.0
+        if isnumber(cmd_char):
+            # rotate by specified angle
+            rotatebot(int(cmd_char))
+        else:
+            if cmd_char == 's':
+                twist.linear.x = 0.0
+                twist.angular.z = 0.0
+            elif cmd_char == 'w':
+                twist.linear.x = 0.1
+                twist.angular.z = 0.0
+            elif cmd_char == 'x':
+                twist.linear.x = -0.1
+                twist.angular.z = 0.0
 
-			pub.publish(twist)
+            pub.publish(twist)
 
         rospy.loginfo(cmd_char)
         rate.sleep()
