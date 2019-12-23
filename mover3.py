b4f69e595198bcd376b16ec6b8ba8b02a5d94654
Shih-Cheng Yen <shihcheng@nus.edu.sg>
2019-12-24 00:30:29 +0800

    Modified to use complex numbers for tracking rotation.

--- mover3.py
+++ mover3.py
@@ -5,6 +5,7 @@ from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Twist
 import math
+import cmath
 
 roll = pitch = yaw = 0.0
 
@@ -32,43 +33,47 @@ def rotatebot(rot_angle):
     # set the update rate to 1 Hz
     rate = rospy.Rate(1)
 
+# Use complex numbers with modulus 1 instead of angles. That would be z = cos(alpha) + i * sin(alpha).
 
+# if (imag(desired / current) > 0.0)
+#   current *= Complex(cos(0.1), sin(0.1));
+# else
+#   current *= Complex(cos(0.1), -sin(0.1));
+
+# It's not just this piece of code: Pretty much anything you want to do with angles is easier with complex numbers.
 
     # get current yaw angle
     current_yaw = yaw
     # log the info
     rospy.loginfo(['Current: ' + str(math.degrees(current_yaw))])
+    c_yaw = complex(math.cos(yaw),math.sin(yaw))
     # calculate desired yaw
     target_yaw = current_yaw + math.radians(rot_angle)
-    desired_yaw = target_yaw % (2*math.pi)
-    rospy.loginfo(['Desired: ' + str(math.degrees(desired_yaw))])
+    c_target_yaw = complex(math.cos(target_yaw),math.sin(target_yaw))
+    rospy.loginfo(['Desired: ' + str(math.degrees(cmath.phase(c_target_yaw)))])
+    # divide the two complex numbers to get the change in direction
+    c_change = c_target_yaw / c_yaw
+    c_change_dir = c_change.imag / cmath.polar(c_change)
     # set linear speed to zero so the TurtleBot rotates on the spot
     twist.linear.x = 0.0
     # check which direction we should rotate
-    if(rot_angle>0):
-        twist.angular.z = 0.1
-        # rotate until yaw angle exceeds yaw+angle
-        pub.publish(twist)
-
-        while(yaw < desired_yaw):
-            rospy.loginfo(['While Yaw: ' + str(math.degrees(yaw))])
-            rate.sleep()
-    else:
-        twist.angular.z = -0.1
-        # rotate until yaw angle exceeds yaw+angle
-        pub.publish(twist)
-
-        while(yaw > desired_yaw):
-            rospy.loginfo(['While Yaw: ' + str(math.degrees(yaw))])
-            rate.sleep()
-
-    rospy.loginfo(['End Yaw: ' + str(math.degrees(yaw))])
-    # change twist object to stop rotation
-    twist.angular.z = 0.0
-
+    twist.angular.z = c_change_dir * 0.1
     # rotate until yaw angle exceeds yaw+angle
     pub.publish(twist)
 
+    c_dir_diff = c_change_dir    
+    while(c_change_dir * c_dir_diff < 0):
+        c_yaw = complex(math.cos(yaw),math.sin(yaw))
+        rospy.loginfo(['While Yaw: ' + str(math.degrees(yaw))])
+        c_change = c_target_yaw / c_yaw
+        c_dir_diff = c_change.imag / cmath.polar(c_change)
+        rate.sleep()
+
+    rospy.loginfo(['End Yaw: ' + str(math.degrees(yaw))])
+    twist.angular.z = 0.0
+    # rotate until yaw angle exceeds yaw+angle
+    pub.publish(twist)
+    
 
 def mover2():
     twist = Twist()
